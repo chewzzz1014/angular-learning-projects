66911df5aba1764217106751601c59552648ce18
chewzzz <chewziqing@gmail.com>
2023-01-21 14:02:17 +0800

    Add multiples lines of inline template

--- hotel-inventory-app/src/app/app.component.ts
+++ hotel-inventory-app/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
-  template: 'Hello World from inline template',
+
+  // use backticks for multiline
+  template: `
+  <h1>Hello World from inline template</h1>
+  <p>Angular is awesome!</p>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
